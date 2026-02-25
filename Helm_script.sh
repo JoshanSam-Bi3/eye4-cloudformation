36f6984c9a1f667ba669a4b7b0d00bbd326ebdc5
Joshua Edwin Rajan A <joshua.anantharajan@bi3technologies.com>
2026-02-25 12:01:41 +0530

    Update Helm_script.sh

--- Helm_script.sh
+++ Helm_script.sh
@@ -20,43 +20,42 @@ AWS_REGION=$(aws configure get region)
 echo "AWS Account ID: $AWS_ACCOUNT_ID"
 echo "AWS Region: $AWS_REGION"
 
-read -p "Enter the Admin ARN(Eg.arn:aws:iam::<ACCOUNT_ID>:user/<user-email>): " param
-
-# CloudFormation rendering 
-aws cloudformation create-stack \
- --stack-name  \
- --template-body https://raw.githubusercontent.com/JoshanSam-Bi3/eye4-cloudformation/refs/heads/main/eks-infra-only-cf.yaml \
- --parameters ParameterKey=AdminIAMArns,ParameterValue="$param" \
- --capabilities CAPABILITY_NAMED_IAM
-
-echo 
-echo "Cloudformation template deployed successfully inside the AWS... 🎉"
-
 # Kubectl Connection
+
+read -p "Enter stack Name to be created: " STACK_NAME
+
+DEPLOYMENT_ID=$(LC_ALL=C tr -dc a-z0-9 </dev/urandom | head -c 16 ; echo)
+
+aws cloudformation create-stack --stack-name eye4-infra-only-stack \
+  --stack-name $STACK_NAME \
+  --template-body raw.githubusercontent.com/JoshanSam-Bi3/eye4-cloudformation/refs/heads/main/eks-infra-only-cf.yaml \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameters ParameterKey=DeploymentID,ParameterValue=$DEPLOYMENT_ID
+
 EKS_CLUSTER_NAME="EksCluster"
 
 aws eks update-kubeconfig --name "$EKS_CLUSTER_NAME" --region "$AWS_REGION"
 
-# Delete exisiting namespaces and resources if they exist
-kubectl delete ns strimzi strimzi-system cnpg-system ingress-nginx cert-manager eye4 --ignore-not-found
-
 # Create Namespace for Kubernetes Pods
-kubectl create ns strimzi eye4
+
+kubectl create ns strimzi
+kubectl create ns eye4 
 
 # Install Operators
+
 helm repo add cnpg https://cloudnative-pg.github.io/charts 
 helm repo update 
 helm install cnpg cnpg/cloudnative-pg --namespace cnpg-system --create-namespace --version 0.27.1 
 
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx 
 helm repo update 
-helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
+helm install ingress-nginx ingress-nginx/ingress-nginx \
   --namespace ingress-nginx \
   --create-namespace 
 
 helm repo add strimzi https://strimzi.io/charts/ 
 helm repo update 
-helm upgrade --install strimzi-operator oci://quay.io/strimzi-helm/strimzi-kafka-operator \
+helm install strimzi-operator oci://quay.io/strimzi-helm/strimzi-kafka-operator \
   --version 0.40.0 \
   -n strimzi-system \
   --set 'watchNamespaces={strimzi}' \
@@ -64,7 +63,7 @@ helm upgrade --install strimzi-operator oci://quay.io/strimzi-helm/strimzi-kafka
 
 helm repo add jetstack https://charts.jetstack.io 
 helm repo update 
-helm upgrade --install cert-manager jetstack/cert-manager \
+helm install cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --create-namespace \
   --version v1.14.4 \
@@ -89,11 +88,13 @@ kubectl apply -f "$secret"
 rm -f "$secret"
 
 # Login into AWS ECR Repo
+
 aws ecr get-login-password --region us-east-1 | helm registry login --username AWS --password-stdin 709825985650.dkr.ecr.us-east-1.amazonaws.com 
 
 read -p "Enter the S3 bucket name for Volume Mount: " BUCKET_NAME
 read -p "Enter the EFS ID for Volume Mount: " EFS_ID
 read -p "Enter the domain name: " DOMAIN_NAME
+read -p "Enter the S3 backup bucket name for CNPG PostgreSQL: " BACKUP_BUCKET_NAME
 
 # AWS Cognito Registration 
 read -p "Enter Pool name for Cognito: " COGNITO_POOL_NAME
@@ -123,22 +124,10 @@ aws cognito-idp create-user-pool-client \
 read -r -p "Enter the username for the Cognito user: " COGNITO_USERNAME
 read -r -p "Enter your email for the Cognito user: " COGNITO_EMAIL
 
-aws cognito-idp update-user-pool-client \
-  --user-pool-id "$COGNITO_POOL_ID" \
-  --client-id "$COGNITO_CLIENT_ID" \
-  --explicit-auth-flows ALLOW_CUSTOM_AUTH ALLOW_REFRESH_TOKEN_AUTH ALLOW_USER_PASSWORD_AUTH ALLOW_USER_SRP_AUTH \
-  --supported-identity-providers COGNITO \
-  --callback-urls "https://$DOMAIN_NAME/api/auth/callback/cognito" \
-  --allowed-o-auth-flows code implicit \
-  --allowed-o-auth-scopes openid email profile \
-  --allowed-o-auth-flows-user-pool-client
-
 # Sanitize inputs: remove CR/LF, trim surrounding whitespace.
 COGNITO_USERNAME=$(printf '%s' "$COGNITO_USERNAME" | tr -d '\r\n' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
 # lower-case the email and trim
 COGNITO_EMAIL=$(printf '%s' "$COGNITO_EMAIL" | tr -d '\r\n' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | tr '[:upper:]' '[:lower:]')
-
-echo "Type username with required parameters (Eg. Joshan) -> No special chars or spaces"
 
 # Basic validation
 if [ -z "${COGNITO_USERNAME}" ] || [ -z "${COGNITO_EMAIL}" ]; then
@@ -155,7 +144,7 @@ aws cognito-idp admin-create-user \
 
 echo "Login into Cognito using the username and temporary password to set a new password for the user."
 
-echo ""
+echo 
 echo "Cognito email ID: $COGNITO_EMAIL"
 echo "Cognito Password: TempPass@123"
 
@@ -167,24 +156,25 @@ aws cognito-idp create-user-pool-domain \
 COGNITO_DOMAIN=$(aws cognito-idp describe-user-pool --user-pool-id "$COGNITO_POOL_ID" --query "UserPool.Domain" --output text --no-cli-pager)
 COGNITO_CLIENT_ID=$(aws cognito-idp list-user-pool-clients --user-pool-id "$COGNITO_POOL_ID" --query "UserPoolClients[?ClientName=='$COGNITO_CLIENT_NAME'].ClientId" --output text --no-cli-pager)
 COGNITO_CLIENT_SECRET=$(aws cognito-idp describe-user-pool-client --user-pool-id "$COGNITO_POOL_ID" --client-id "$COGNITO_CLIENT_ID" --query "UserPoolClient.ClientSecret" --output text --no-cli-pager)
-
 # demo.eye4.ai.auth.ap-southeast-2.amazoncognito.com
 
 NEXT_AUTH_SECRET=$(openssl rand -base64 32)
 
 helm install eye4-release oci://709825985650.dkr.ecr.us-east-1.amazonaws.com/bi3-technologies/eye4 \
-  --version 0.3.0 \
+  --version 0.4.0 \
   --namespace eye4 \
   --set eye4-storage.s3.accountId=$AWS_ACCOUNT_ID \
   --set eye4-storage.s3.volumeHandle=$BUCKET_NAME \
   --set eye4-storage.efs.volumeHandle=$EFS_ID \
   --set global.domain=$DOMAIN_NAME \
+  --set cnpg-postgresql.postgresql.cluster.backup.barmanObjectStore.s3Bucket="$BACKUP_BUCKET_NAME" \
+  --set cnpg-postgresql.postgresql.cluster.backup.barmanObjectStore.destinationPath="s3://${BACKUP_BUCKET_NAME}/eye4" \
   --set eye4-frontend.env.NEXT_PUBLIC_COGNITO_CLIENT_ID="$COGNITO_CLIENT_ID" \
-  --set eye4-frontend.env.NEXT_PUBLIC_COGNITO_POOL_ID="$COGNITO_POOL_ID" \
+  --set eye4-frontend.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID="$COGNITO_POOL_ID" \
   --set eye4-frontend.env.NEXT_PUBLIC_COGNITO_REGION="$AWS_REGION" \
   --set eye4-frontend.env.NEXT_PUBLIC_COGNITO_DOMAIN="$COGNITO_DOMAIN" \
   --set eye4-frontend.env.NEXT_PUBLIC_COGNITO_CLIENT_SECRET="$COGNITO_CLIENT_SECRET" \
-  --set eye4-frontend.env.NEXT_PUBLIC_NEXT_AUTH_SECRET="$NEXT_AUTH_SECRET"; echo "Eye4 Helm Chart deployed successfully!"
+  --set eye4-frontend.env.NEXTAUTH_SECRET="$NEXT_AUTH_SECRET"; echo "Eye4 Helm Chart deployed successfully!"
   
 pg_config="$(mktemp)"
 trap 'rm -f "$pg_config"' EXIT
